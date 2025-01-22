104bf5d94fc404368d0be5c04cad79709cbc7030
niharikamanhar <niharikamanhar476@gmail.com>
2025-01-22 21:49:44 +0530

    Adding files for website

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# Calculator Website
+A simple calculator built using HTML, CSS, and JavaScript that allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division. The calculator also supports keyboard input for a more seamless user experience.
+
+## Technologies Used
+- **HTML**: For creating the structure of the webpage.
+- **CSS**: For styling the calculator layout and making it visually appealing.
+- **JavaScript**: For implementing the logic and functionality of the calculator.
+
+## Installation
+1. Clone this repository to your local machine
+```
+git clone https://github.com/niharika-2212/Calculator-website.git
+```
+2. Navigate to the project directory
+```
+cd <repo-name>
+```
+3. Open index.html in your browser to see the calculator in action.
+
+## How to Use
+- Click on the buttons to input numbers and perform operations.
+- You can also use your keyboard to input numbers and operations:
+  - Number keys (0-9) for numbers.
+  - +, -, *, / for arithmetic operations.
+  - = to calculate the result.
+- The result will be displayed in the output field.
+
+## Live Demo
+You can view the website here
---
+++ script.js
@@ -0,0 +1,90 @@
+// initialize variables
+var last_val = 0;
+var cur_val = 0;
+var final = 0;
+var operation = 0;
+
+/*
+on each value and operator click
+- display the value in space
+- save it
+*/
+
+function inputNumber(value){
+  document.querySelector("p").innerHTML = value;
+  cur_val = parseInt(value);
+}
+for (var i = 0; i < 10; i++) {
+  document.querySelectorAll(".btn1")[i].addEventListener("click", function () {
+    inputNumber(this.innerHTML);
+    addClass(this);
+  });
+}
+
+document.querySelector("body").addEventListener("keydown",function(event){
+  if(event.key==="0" || event.key==="1" || event.key==="2" || event.key==="3" || event.key==="4" || event.key==="5" || event.key==="6" || event.key==="7" || event.key==="8" || event.key==="9"){
+    inputNumber(event.key);
+  }
+})
+
+function inputOperations(value){
+  document.querySelector("p").innerHTML = value;
+  last_val = cur_val;
+  operation = value;
+}
+
+for (var i = 0; i < 4; i++) {
+  document.querySelectorAll(".btn2")[i].addEventListener("click", function () {
+    inputOperations(this.innerHTML);
+    addClass(this);
+  });
+}
+
+document.querySelector("body").addEventListener("keydown",function(event){
+  if(event.key==="+" || event.key==="-" || event.key==="/" || event.key==="*"){
+    inputOperations(event.key);
+  }
+})
+
+document.querySelector(".equal").addEventListener("click", function () {
+  calculate();
+  addClass(document.querySelector(".equal"));
+});
+
+document.querySelector("body").addEventListener("keydown",function(event){
+  if(event.key==="=" || event.key==="Enter"){
+    calculate();
+    addClass(document.querySelector(".equal"));
+  }
+})
+
+document.querySelector(".clear").addEventListener("click", function () {
+  document.querySelector("p").innerHTML = 0;
+  var last_val = 0;
+  var cur_val = 0;
+  var final = 0;
+  var operation = 0;
+  addClass(document.querySelector(".clear"));
+});
+
+function calculate() {
+  if (operation === "+") {
+    final = last_val + cur_val;
+  } else if (operation === "-") {
+    final = last_val - cur_val;
+  } else if (operation === "x") {
+    final = last_val * cur_val;
+  } else if (operation === "/") {
+    final = last_val / cur_val;
+  }
+  last_val = final;
+  cur_val = final;
+  document.querySelector("p").innerHTML = final;
+}
+
+function addClass(element) {
+  element.classList.add("pressed");
+  setTimeout(() => {
+    element.classList.remove("pressed");
+  }, 100);
+}
